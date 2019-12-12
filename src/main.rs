edc96037ca4d48e70033c4a64044b76efedec1ad
Charles Saracco <crsaracco@gmail.com>
2019-12-12 00:58:52 -0500

    jack + oscilloscope

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 use std::collections::VecDeque;
 
-use bounded_spsc_queue::{Producer, Consumer};
+use crossbeam_channel::bounded;
 use hound::WavReader;
 use druid::{AppLauncher, WindowDesc, Data, Lens, Widget};
 use itertools::Itertools;
@@ -16,19 +16,49 @@ struct State {
 }
 
 fn main() {
-    let (p, c) = bounded_spsc_queue::make(4800);
-    thread::spawn(move|| {
-        let mut reader = WavReader::open("/home/crs/Downloads/lifeformed.wav").unwrap();
+    let (p, c) = bounded(4800);
+    let (p2, c2) = bounded(4800);
+
+    thread::spawn(move || {
+        let mut reader = WavReader::open("/home/crs/Downloads/nanou2.wav").unwrap();
         let samples: hound::WavSamples<'_, std::io::BufReader<std::fs::File>, i16> = reader.samples();
-        // Samples are interleaved, so take every other one.
         for s in samples.step_by(2) {
             let val = s.unwrap();
             let val_f32 = (val as f64) / (std::i16::MAX as f64);
             //println!("{}", val_f32);
-            p.push(val_f32);
+            p.send(val_f32);
+            p2.send(val_f32);
         }
     });
 
+    // 1. open a client
+    let (client, _status) =
+        jack::Client::new("rust_jack_sine", jack::ClientOptions::NO_START_SERVER).unwrap();
+
+    // 2. register port
+    let mut out_port = client
+        .register_port("sine_out", jack::AudioOut::default())
+        .unwrap();
+
+    // 3. define process callback handler
+    let process = jack::ClosureProcessHandler::new(
+        move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+            // Get output buffer
+            let out = out_port.as_mut_slice(ps);
+
+            // Write output
+            for v in out.iter_mut() {
+                *v = c2.recv().unwrap() as f32;
+            }
+
+            // Continue as normal
+            jack::Control::Continue
+        },
+    );
+
+    // 4. activate the client
+    let active_client = client.activate_async((), process).unwrap();
+    // processing starts here
 
     let state = State {
         some_value: 42,
--- src/oscilloscope.rs
+++ src/oscilloscope.rs
@@ -3,32 +3,53 @@ use std::sync::{Arc, Mutex};
 use druid::{Widget, EventCtx, PaintCtx, BoxConstraints, BaseState, LayoutCtx, Event, Env, UpdateCtx};
 use druid::piet::{Color, RenderContext};
 use druid::kurbo::{Line, Point, Size};
+use log::*;
 
-use bounded_spsc_queue::{Producer, Consumer};
+use crossbeam_channel::{Sender, Receiver};
 
 pub struct Oscilloscope {
-    consumer: Consumer<f64>,
+    consumer: Receiver<f64>,
     buffer: VecDeque<f64>,
+    t: f64,
+    sum_t: f64,
 }
 
 use super::State;
 use std::collections::VecDeque;
 
 impl Oscilloscope {
-    pub fn new(consumer: Arc<Mutex<Option<Consumer<f64>>>>) -> Self {
+    pub fn new(consumer: Arc<Mutex<Option<Receiver<f64>>>>) -> Self {
         let mut c = consumer.lock().unwrap();
         let mut new_consumer = None;
         std::mem::swap(&mut *c, &mut new_consumer);
         Self {
             consumer: new_consumer.unwrap(),
             buffer: VecDeque::new(),
+            t: 0.0,
+            sum_t: 0.0,
         }
     }
 }
 
 impl Widget<State> for Oscilloscope {
-    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut State, _env: &Env) {
-        // Don't handle events
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, _data: &mut State, _env: &Env) {
+        match event {
+            Event::MouseDown(_) => {
+                self.t = 0.0;
+                self.sum_t = 0.0;
+                ctx.request_anim_frame();
+            }
+            Event::AnimFrame(interval) => {
+                let delta_t = (*interval as f64) * 1e-9;
+                self.t = delta_t;
+                self.sum_t += delta_t;
+                ctx.request_anim_frame();
+                // When we do fine-grained invalidation,
+                // no doubt this will be required:
+                //ctx.invalidate();
+            }
+            _ => (),
+        }
     }
 
     fn update(&mut self, _ctx: &mut UpdateCtx, _old_data: Option<&State>, _data: &State, _env: &Env) {
@@ -41,15 +62,14 @@ impl Widget<State> for Oscilloscope {
     }
 
     fn paint(&mut self, paint_ctx: &mut PaintCtx, base_state: &BaseState, data: &State, env: &Env) {
+        let buffer_size = 8192;
         // Consume some samples
-        for i in 0..100 {
-            let x = self.consumer.pop();
+        for i in 0..(48000.0 * self.t) as usize {
+            let x = self.consumer.recv().unwrap();
             self.buffer.push_back(x);
-        }
-
-        // Limit buffer to 800 samples
-        while self.buffer.len() > 800 {
-            self.buffer.pop_front();
+            if self.buffer.len() > buffer_size {
+                self.buffer.pop_front();
+            }
         }
 
         // Redraw
@@ -57,11 +77,13 @@ impl Widget<State> for Oscilloscope {
 
         // Draw all of the samples we have so far
         let red = Color::from_rgba32_u32(0xff0000ff);
-        for x in 0..(self.buffer.len()-1) {
-            let p0 = Point::new(x as f64, self.buffer[x] * 300.0 + 300.0);
-            let p1 = Point::new((x+1) as f64, self.buffer[x+1] * 300.0 + 300.0);
-            let line = Line::new(p0, p1);
-            paint_ctx.stroke(line, &red, 1.0);
+        if self.buffer.len() > 0 {
+            for x in 0..(self.buffer.len()-1) {
+                let p0 = Point::new(x as f64 * 800.0 / (buffer_size as f64), self.buffer[x] * 300.0 + 300.0);
+                let p1 = Point::new((x+1) as f64 * 800.0 / (buffer_size as f64), self.buffer[x+1] * 300.0 + 300.0);
+                let line = Line::new(p0, p1);
+                paint_ctx.stroke(line, &red, 1.0);
+            }
         }
     }
 }
